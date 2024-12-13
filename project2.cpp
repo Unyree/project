b2c38a119c05e8f2b8d9fddeddb6b54669a0e182
Vika <vikamyranova506@gmail.com>
2024-12-13 22:25:28 +0700

    добавила ветвь

--- project2.cpp
+++ project2.cpp
@@ -4,6 +4,7 @@ int main()
 {
     std::cout << "Hello World!\n";//коммент х1
     std::cout << "Za Worldo00000!\n";//коммент х2
+    std::cout << "Za Worldo00000!\n";//коммент х3
     //Vika comment
 }
 
